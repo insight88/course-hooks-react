8b87ddb278f8991e9418f134505bb79eb1b707a5
insight88 <kpkim1988@gmail.com>
2020-09-10 14:07:36 +0900

    useMemo

---
+++ src/useCallback/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useCallback } from 'react';
+import { Hello } from './Hello';
+import { Square } from './Square';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const favouriteNums = [7, 21, 37];
+
+  const increment = useCallback(
+    (n) => {
+      setCount((c) => c + n);
+    },
+    [setCount]
+  );
+  // * useCallback(callbackFn, [deps])
+  // * shouldComponentUpdate와 비슷하게, deps가 바뀔 때만 callback을 부름
+
+  return (
+    <div>
+      <Hello increment={increment} />
+      <div>count: {count}</div>
+      {favouriteNums.map((n) => {
+        return <Square increment={increment} n={n} key={n} />;
+      })}
+    </div>
+  );
+};
+
+export default App;
